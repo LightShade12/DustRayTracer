9244e144bed8020bd41044d3c450a84c688ac245
LightShade12 <subhamswostikpradhan2004@gmail.com>
2024-08-01 22:04:01 +0530

    init diffuse bsdf pre ckpt

--- DustRayTracer/src/Core/PrincipledBSDF.cu
+++ DustRayTracer/src/Core/PrincipledBSDF.cu
@@ -1,1 +1,62 @@
 #include "PrincipledBSDF.cuh"
+#include "CudaMath/physical_units.hpp"
+#include "CudaMath/Random.cuh"
+
+__device__ float3 sampleCosineHemisphere(float2 u) {
+	const float phi = u.x * 2.0f * PI;
+	const float sqrtr2 = sqrt(u.y);
+	const float x = cos(phi) * sqrtr2;
+	const float y = sin(phi) * sqrtr2;
+	const float z = sqrt(1.0f - u.y);
+
+	return make_float3(x, y, z);
+}
+
+__device__ PrincipledBSDF::PrincipledBSDF(
+	float3 albedo,
+	float metallic,
+	float roughness,
+	float transmission,
+	float ior) :
+	m_albedo(albedo),
+	m_metallicity(metallic),
+	m_roughness(roughness),
+	m_transmission(transmission),
+	m_ior(ior)
+{}
+__device__ float3 PrincipledBSDF::f(const float3& wo, const float3& wi)
+{
+	float3 val;
+	val += fOpaqueDielectric(wo, wi, m_albedo);
+
+	return val;
+}
+__device__ BSDFSample PrincipledBSDF::sample_f(const float3& wo, uint32_t& seed)
+{
+	BSDFSample sample;
+
+	sample = sampleOpaqueDielectric(wo, m_albedo, seed);
+
+	return sample;
+}
+
+__device__ float3 PrincipledBSDF::fOpaqueDielectric(const float3& wo, const float3& wi, const float3& albedo)
+{
+	return albedo / PI;
+}
+
+__device__ BSDFSample PrincipledBSDF::sampleOpaqueDielectric(const float3& wo, const float3& albedo, uint32_t& seed)
+{
+	float2 u = make_float2(randomFloat(seed), randomFloat(seed));
+	float3 wi = sampleCosineHemisphere(u);
+	float cDiffuse = 1 / PI;
+	return BSDFSample(BSDFSample::Diffuse | BSDFSample::Reflected,
+		fOpaqueDielectric(wo, wi, m_albedo),
+		wi,
+		pdfOpaqueDielectric(wo, wi));
+}
+
+__device__ float PrincipledBSDF::pdfOpaqueDielectric(const float3& wo, const float3& wi)
+{
+	return abs(wi.y) * 1 / PI;
+}
--- DustRayTracer/src/Core/PrincipledBSDF.cuh
+++ DustRayTracer/src/Core/PrincipledBSDF.cuh
@@ -1,5 +1,51 @@
 #pragma once
 
-class PrincipledBSDF {
+#include "CudaMath/helper_math.cuh"
 
+struct BSDFSample {
+	enum Scatter {
+		Absorbed = 0,
+		Emitted = 1,
+		Reflected = 2,
+		Transmitted = 4,
+		Diffuse = 8,
+		Glossy = 16,
+		Specular = 32
+	};
+	__device__ BSDFSample() = default;
+	__device__ BSDFSample(int scatter, float3 f, float3 wi, float pdf) :scatter(scatter), f(f), wi(wi), pdf(pdf) {};
+
+	int scatter;      // Scatter flags
+	float3 f;         // BSDF value
+	float3 wi;        // Sampled incident light direction
+	float pdf;        // Sampled direction PDF
+
+	[[nodiscard]] constexpr bool is(int flag) const noexcept {
+		return scatter & flag;
+	}
+};
+
+class PrincipledBSDF {
+	__device__ PrincipledBSDF(float3 albedo = make_float3(1, 0, 1),
+		float metallic = 0,
+		float roughness = 0.5,
+		float transmission = 0,
+		float ior = 1.5);
+
+	__device__ float3 f(const float3& wo, const float3& wi);
+
+	__device__ BSDFSample sample_f(const float3& wo, uint32_t& seed);
+
+	__device__ float3 fOpaqueDielectric(const float3& wo, const float3& wi, const float3& albedo);
+
+	__device__ BSDFSample sampleOpaqueDielectric(const float3& wo, const float3& albedo, uint32_t& seed);
+
+	__device__ float pdfOpaqueDielectric(const float3& wo, const float3& wi);
+
+private:
+	float3 m_albedo;
+	float m_metallicity = 0;
+	float m_roughness = 0.5;
+	float m_transmission = 0;
+	float m_ior = 1.5;
 };
